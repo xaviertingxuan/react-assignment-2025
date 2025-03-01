d958c54c7150e12340324f9db99d881b33d3cfff
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-01 12:13:46 +0800

    navbar&usestate

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ProductCard = (props) => {
+
+    const [cart, setCart] = useState(0)
+
+    const handleAddToCart = () => {
+        setCart(c => c + 1)
+    }
+
     return (
         <div>
             <div className="card">
@@ -12,7 +19,8 @@ const ProductCard = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{props.productName}</h5>
                     <p className="card-text">${props.price}</p>
-                    <button className="btn btn-primary">
+                    <p className="card-text">Items in cart: {cart}</p>
+                    <button className="btn btn-primary" onClick={handleAddToCart}>
                         Add to Cart
                     </button>
                 </div>
