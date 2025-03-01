1c40b2b8b631975aca11449a72951defb3ca55fc
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-01 10:03:54 +0800

    react created

---
+++ React-assignment/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="div1"></div>
+      <div className="div2"></div>
+      <div className="div3"></div>
+    </>
+  )
+}
+
+export default App
