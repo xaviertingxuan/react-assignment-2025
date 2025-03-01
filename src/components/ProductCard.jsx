542046ce3e4cc67d8b9b95c954df36805a23bbb1
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-01 14:36:03 +0800

    update to router

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,6 +8,11 @@ const ProductCard = (props) => {
         setCart(c => c + 1)
     }
 
+    //prevent cart from going below 0
+    const handleRemovefromCart = () => {
+        setCart(c => Math.max(0, c - 1))
+    }
+
     return (
         <div>
             <div className="card">
@@ -20,9 +25,15 @@ const ProductCard = (props) => {
                     <h5 className="card-title">{props.productName}</h5>
                     <p className="card-text">${props.price}</p>
                     <p className="card-text">Items in cart: {cart}</p>
+                    <div className="d-flex justify-content-between d-grid gap-3">
                     <button className="btn btn-primary" onClick={handleAddToCart}>
                         Add to Cart
                     </button>
+                    <button className="btn btn-danger" onClick={handleRemovefromCart}>
+                        Remove from Cart
+                    </button>
+
+                    </div>
                 </div>
             </div>
         </div>
