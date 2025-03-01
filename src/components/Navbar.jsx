4ce29d740c5a6d50d8d88fec45d49dd2c482c2cb
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-01 12:18:48 +0800

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,39 +4,40 @@ const Navbar = () => {
 
   const [isNavbarShowing, setNavbarShowing] = useState(false)
 
-  
-  
+
+
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container">
-      <a className="navbar-brand" href="#">E-Shop</a>
-      <button
-        className="navbar-toggler"
-        type="button"
-         onClick={() => setNavbarShowing(!isNavbarShowing)}
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`} 
-         id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Products</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
+      <div className="container">
+        <a className="navbar-brand" href="#">E-Shop</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+
+          onClick={() => setNavbarShowing(!isNavbarShowing)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`}
+          id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Products</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
