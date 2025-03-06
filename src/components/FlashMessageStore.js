6ca7f1f096d306b8f5a542c8c1d320558dfc2197
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-06 19:49:55 +0800

    update with notes

--- src/components/FlashMessageStore.js
+++ src/components/FlashMessageStore.js
@@ -1,5 +1,9 @@
 import { atom, useAtom } from "jotai";
 
+//atom is a state container that can be read from and written to by multiple components
+//useAtom is a hook that returns the current value of an atom and a function to update it
+//flashMessageAtom is an atom that stores the message and type of the flash message
+//atom is a piece of data that is going to be shared
 
 export const flashMessageAtom = atom({
     message: '',
@@ -7,20 +11,30 @@ export const flashMessageAtom = atom({
 });
 
 export const useFlashMessage = () => {
+
+    // get the amtom and the mutator function to update the atom
+    //which shared data to update or to get from --> flashMessageAtom
     const [flashMessage, setFlashMessage] = useAtom(flashMessageAtom);
+
+    //helper function: a function that makes it easier to do something
+
+    //set which message to show and its type
+
+    //when a function parameter has a "=xyz" it means that if the parameter is not passed, it will be set to "xyz", then xyz will be the default value
 
     const showMessages = (message, type = 'info') => {
         setFlashMessage ({message, type});
-    }
-
-    const getMessage = () => {
-        return flashMessage;
     }
 
     const clearMessage = () => {
         setFlashMessage({ message: '', type: 'info' });
     };
 
+    const getMessage = () => {
+        return flashMessage;
+    }
+
+    //returning objects
    return {
         getMessage,
         showMessages,
