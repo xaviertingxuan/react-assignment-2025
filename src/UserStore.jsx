4f68f6fdd47f969cb4dd5e8f076a6c2707ff30c5
xaviertingxuan <xaviertingxuan@gmail.com>
2025-03-15 15:50:30 +0800

    update

---
+++ src/UserStore.jsx
@@ -0,0 +1,10 @@
+// import create from 'zustand'
+
+// const useJwt = create((set) => ({
+//   jwt: null,
+//   setJwt: (token) => set({ jwt: token }),
+//   getJwt: () => useJwt.getState().jwt,
+//   clearJwt: () => set({ jwt: null }) // Add this line for logout
+// }))
+
+// export { useJwt }
